e71a6e8818c5979cf59a3f9554dc677403a3940c
saharabanu <saharabanungn@gmail.com>
2022-04-22 14:45:57 +0600

    add

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
+        <div className='bg-gray text-center fs-1'>
             <h3>Page Not Found</h3>
             <Link to='/'>Go  Home</Link>
         </div>
